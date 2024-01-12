7f3a7a73f289edbb22bcee70247995cd22ace74e
carolgs <goncalves.macarolina@gmail.com>
2024-01-12 15:33:56 -0300

    feat: form validation script

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    // Event
+    let input = document.querySelector(".input-btn");
+    input.addEventListener("click", (event) => {
+        event.preventDefault();
+        formValidation();
+    });
+    // Function
+    function formValidation() {
+        let input = document.querySelector(".input");
+        let span = document.querySelector(".span");
+        let inputBtn = document.querySelector(".input-btn");
+        // Conditional statement for toggling classes
+        if (input.value === "") {
+            input.classList.add("active");
+            span.classList.remove("inactive");
+            span.classList.add("active");
+            inputBtn.classList.add("active");
+            return false;
+        } else {
+            input.classList.remove("active");
+            span.classList.remove("active");
+            span.classList.add("inactive");
+            inputBtn.classList.remove("active");
+        }; 
+        return true;
+    };
+};
