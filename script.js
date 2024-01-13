bc3ce45d3970e311a7f74341b4556929ce862d3e
carolgs <goncalves.macarolina@gmail.com>
2024-01-13 18:04:01 -0300

    feat: error icon

--- script.js
+++ script.js
@@ -8,17 +8,20 @@ window.onload = function() {
     // Function
     function formValidation() {
         let input = document.querySelector(".input");
+        let errorIcon = document.querySelector(".error-icon");
         let span = document.querySelector(".span");
         let inputBtn = document.querySelector(".input-btn");
         // Conditional statement for toggling classes
         if (input.value === "") {
             input.classList.add("active");
+            errorIcon.classList.add("active");
             span.classList.remove("inactive");
             span.classList.add("active");
             inputBtn.classList.add("active");
             return false;
         } else {
             input.classList.remove("active");
+            errorIcon.classList.remove("active");
             span.classList.remove("active");
             span.classList.add("inactive");
             inputBtn.classList.remove("active");
